86131febb5b115ba37d678892b77f6fd8c056ed9
Elise Phan <aliceconnic@gmail.com>
2019-04-26 19:43:32 +0000

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,14 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+     return state.concat(action.quote);
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quoteId !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      return 
+    default:
+      return state
+  }
+  
+  
 }
